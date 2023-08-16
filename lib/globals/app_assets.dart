70fed1661593de242256b2366c05927cda0e89ef
faysalneowaz <faysalneowaz@gmail.com>
2023-08-16 16:47:36 +0600

    changed AppAssets

---
+++ lib/globals/app_assets.dart
@@ -0,0 +1,64 @@
+class AppAssets {
+  static String facebook = 'assets/images/face.png';
+  static String github = 'assets/images/git.png';
+  static String linkedIn = 'assets/images/in.png';
+  static String insta = 'assets/images/insta.png';
+  static String twitter = 'assets/images/twit.png';
+  static String profile1 = 'assets/images/pro1.png';
+  static String profile2 = 'assets/images/pro2.png';
+  static String code = 'assets/images/coding.png';
+  static String brush = 'assets/images/brush-stroke.png';
+  static String analyst = 'assets/images/analytics.png';
+  static String work1 = 'assets/images/work1.jpg';
+  static String work2 = 'assets/images/work2.jpg';
+  static String share = 'assets/images/share.png';
+  static String menuItemHome = 'Home';
+  static String menuItemAbout = 'About';
+  static String menuItemServices = 'Services';
+  static String menuItemPortfolio = 'Portfolio';
+  static String menuItemContact = 'Contact';
+  static String aboutmeTitle = 'Flutter Developer!';
+  static String homepageHelloText = 'Hello, It\'s Me';
+  static String homepageName = 'Mukhtar Ali Khan';
+  static String homepageSpanCons = 'And I\'m a ';
+  static String homepageSpan1 = 'Flutter & Android Developer';
+  static String homepageSpan1mobile = 'Flutter & \nAndroid \nDeveloper';
+  static String homepageSpan2 = 'Freelancer';
+  static String homepageSpan3 = 'YouTuber';
+  static String homepageDownloadCv = 'Download CV';
+  static String readMoretext = 'Read More';
+  static String aboutmeSpan1 = 'About ';
+  static String aboutmeSpan2 = 'Me!';
+  static String fullNameHint = 'Full Name';
+  static String emailHint = 'Email Address';
+  static String cellPhoneHint = 'Mobile Number';
+  static String emailSubHint = 'Email Subject';
+  static String messageHint = 'Your Message';
+  static String sendmessagetext = 'Send Message';
+  static String contactmeSpan1 = 'Contact ';
+  static String contactmeSpan2 = 'Me!';
+  static String appdeveTitle = 'App Development';
+  static String graphicdesignTitle = 'Graphic Designing';
+  static String digitalmarketingTitle = 'Digital Marketing';
+  static String serviceSpan1 = 'My ';
+  static String serviceSpan2 = 'Services';
+  static String projectsSpan1 = 'Latest ';
+  static String projectsSpan2 = 'Projects';
+  static String service1shortDesc =
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+      ' The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,';
+  static String appdeveshortDesc =
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.';
+  static String homepageShortDesc =
+      'In publishing and graphic design, Lorem ipsum is a placeholder '
+      'text commonly used to demonstrate the visual form of a document'
+      ' or a typeface without relying on meaningful content.';
+  static String aboutmeShortdesc =
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+      ' The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,'
+      ' as opposed to using \'Content here, content here\', making it look like readable English.'
+      ' Many desktop publishing packages and web page editors now use Lorem Ipsum as their default'
+      ' model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy.'
+      ' Various versions have evolved over the years, sometimes by accident, sometimes on purpose'
+      ' (injected humour and the like).';
+}
